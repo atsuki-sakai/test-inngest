277c62592244705c0545f8fed8c49c96256c1caf
Atsuki Sakai <atk721@icloud.com>
2025-07-21 23:40:56 +0900

    refactor

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,7 +4,6 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-import { URL } from 'node:url';
 import { calculateInstagramRelevance } from '@/services/instagramExtractor';
 
 // ======================= ユーティリティ関数 ========================
